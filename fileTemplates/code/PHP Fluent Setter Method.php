7f59f9f63a8f56d815dbd1c0c5c48ae857a04e51
Thinmy Patrick Alves <thinmy.alves@saedigital.com.br>
2018-02-22 15:32:13 -0300

    PS-2017.3.2 <thinmy.alves@SAEINF-NT014.saedigital.local Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, diff.xml, Default copy.xml, other.xml, Default.xml, file.template.settings.xml, keymap.xml, code.style.schemes, find.xml, ui.lnf.xml, filetypes.xml, php.xml, remote-servers.xml, code.style.schemes.xml, colors.scheme.xml, notifications.xml, Laravel - Renato.icls, git.xml, databaseDrivers.xml, laravel.icls, Default.xml, Renato.icls, PHP Fluent Setter Method.php, laf.xml, Default _1_.xml, web-browsers.xml, vcs.xml, databaseSettings.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/code/PHP Fluent Setter Method.php
@@ -0,0 +1,10 @@
+/**
+ * @codeCoverageIgnore
+ * @param ${TYPE_HINT} $${PARAM_NAME}
+ * @return ${CLASS_NAME}
+ */
+public function set${NAME}($${PARAM_NAME})
+{
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+    return $this;
+}
